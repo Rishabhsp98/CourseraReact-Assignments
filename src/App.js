57c4f2090422570c61ce09cda916f931f677b2aa
Rishabhsp98 <rish6501@gmail.com>
2020-09-10 19:57:39 +0530

    Images added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 // import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
-import './App.css';
+import Menu from './components/MenuComponent';
+
 
 function App() {
   return (
@@ -10,8 +11,8 @@ function App() {
         <div className="container">
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
-
       </Navbar>
+      <Menu/>
     </div>
   );
 }
